17a3bd4bade785d8e4636827547d44c4c3bddb9c
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-02-23 17:43:34 +0530

    task controllers boilerplate

---
+++ src/controllers/task.controllers.js
@@ -0,0 +1,77 @@
+
+const getTasks = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const createTask = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const updateTask = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const getTaskById = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const deleteTask = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const createSubTask = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const deleteSubTask = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+
+const updateSubTask = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+
+export {
+    createSubTask,
+    createTask,
+    deleteTask,
+    deleteSubTask,
+    getTaskById,
+    getTasks,
+    updateSubTask,
+    updateTask,
+}
