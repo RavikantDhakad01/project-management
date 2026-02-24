640a66abd7d806b07792fb1d20d5aa8f116d80e5
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-02-24 11:01:23 +0530

    add get tasks controller

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -1,6 +1,22 @@
+import { Project } from "../models/project.model.js"
+import { apiErrors } from "../utils/Api-errors.js"
+import ApiResponse from "../utils/Api-Response.js"
+import { Task } from "../models/task.model.js"
 
 const getTasks = async (req, res, next) => {
     try {
+
+        const { projectId } = req.body
+        const project = await Project.findById(projectId)
+
+        if (!project) {
+            throw new apiErrors(404, "project not found")
+        }
+        const tasks = await Task.findOne({
+            project: new mongoose.Types.ObjectId(projectId)
+        }).populate("assignedTo", "avatar username fullName")
+
+        return res.status(200).json(new ApiResponse(200, tasks, "tasks fetched successfully"))
 
     } catch (error) {
         return next(error)
@@ -15,6 +31,15 @@ const createTask = async (req, res, next) => {
     }
 }
 
+const getTaskById = async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        return next(error)
+    }
+}
+
+
 const updateTask = async (req, res, next) => {
     try {
 
@@ -23,13 +48,6 @@ const updateTask = async (req, res, next) => {
     }
 }
 
-const getTaskById = async (req, res, next) => {
-    try {
-
-    } catch (error) {
-        return next(error)
-    }
-}
 
 const deleteTask = async (req, res, next) => {
     try {
